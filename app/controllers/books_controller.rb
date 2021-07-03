24fdbba76c06692bb407d502c33da67ae407be39
JINJIN1103 <ec2-user@ip-172-31-55-96.ec2.internal>
2021-07-03 08:46:38 +0000

    応用課題3 Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-     unless 
+     unless
     @book.user == current_user
      redirect_to books_path
      end
